dce0b4cbf3e94ef9781f559fae385a145be46cb7
Abdelrahman Mostafa <boboelahlawy1234@hotmail.com>
2024-09-15 12:48:32 +0300

    Array of dogs

--- Week1/Lecture2/src/DogLauncher.java
+++ Week1/Lecture2/src/DogLauncher.java
@@ -2,5 +2,10 @@ public class DogLauncher {
     public static void main(String[] args) {
         Dog maya = new Dog(40);
         maya.makeNoise();
+        Dog[] dogs = new Dog[2];
+        dogs[0] = new Dog(3);
+        dogs[1] = new Dog(20);
+        dogs[0].makeNoise();
+        dogs[1].makeNoise();
     }
 }
