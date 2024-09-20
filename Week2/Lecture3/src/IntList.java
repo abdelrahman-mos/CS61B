6771e168ea8ec531bb8e985684a85d2c6b235d37
Abdelrahman Mostafa <boboelahlawy1234@hotmail.com>
2024-09-20 16:17:01 +0300

    Adding recursive get method

--- Week2/Lecture3/src/IntList.java
+++ Week2/Lecture3/src/IntList.java
@@ -1,4 +1,3 @@
-import java.util.*;
 public class IntList {
     public int first;
     public IntList next;
@@ -23,16 +22,25 @@ public class IntList {
         return size;
     }
 
+    public int get(int index) {
+        if (index < 0 || index >= size()) throw new IndexOutOfBoundsException();
+        if (index == 0) return first;
+        return next.get(index-1);
+    }
+
     public static void main(String[] args) {
         IntList list = new IntList(15, null);
         list = new IntList(10, list);
         list = new IntList(5, list);
 
-        for (IntList i = list; i != null; i = i.next) {
-            System.out.println(i.first);
-        }
+//        for (IntList i = list; i != null; i = i.next) {
+//            System.out.println(i.first);
+//        }
 
         System.out.println(list.size());
         System.out.println(list.iterativeSize());
+        System.out.println(list.get(0));
+        System.out.println(list.get(1));
+        System.out.println(list.get(2));
     }
 }
